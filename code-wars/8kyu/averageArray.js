b8462ab86eb3677896fde57e5b80884611c8a8d1
NickSramcik <102764026+NickSramcik@users.noreply.github.com>
2022-04-14 00:36:09 -0400

    Delete code-wars directory

--- code-wars/8kyu/averageArray.js
+++
@@ -1,8 +0,0 @@
-function find_average(array) {
-    let total = 0 
-    if (array === []) return 0
-    else {
-        total = array.forEach(num => total += num)
-        return total / array.length;
-    }
-  }
